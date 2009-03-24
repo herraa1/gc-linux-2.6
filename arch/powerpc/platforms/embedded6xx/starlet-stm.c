b25e2a056637b3b4c912b72ae0955eb432529204
Albert Herranz <albert_herranz@yahoo.es>
2009-03-24 20:08:26 +0100

    wii: starlet/ios: use polled ipc api on platform restart
    This patch fixes some known restart failure scenarios by switching the
    platform restart code to the new polled IPC call interface.
    
    Signed-off-by: Albert Herranz <albert_herranz@yahoo.es>

--- arch/powerpc/platforms/embedded6xx/starlet-stm.c
+++ arch/powerpc/platforms/embedded6xx/starlet-stm.c
@@ -27,6 +27,8 @@
 #define STARLET_STM_HOTRESET	0x2001
 #define STARLET_STM_SHUTDOWN	0x2003
 
+#define STARLET_STM_TIMEOUT	1000000	/* usecs */
+
 #define STARLET_DEV_STM_IMMEDIATE	"/dev/stm/immediate"
 
 #define drv_printk(level, format, arg...) \
@@ -44,17 +46,22 @@ static u32 starlet_stm_buf[(STARLET_IPC_DMA_ALIGN+1)/sizeof(u32)]
  */
 static void starlet_stm_common_restart(int request, u32 value)
 {
+	static int already_in_restart;
 	u32 *buf = starlet_stm_buf;
 	size_t len = sizeof(starlet_stm_buf);
 	int fd;
 	int error;
 
-	/* REVISIT, use polled ipc calls here */
-
+	if (already_in_restart) {
+		drv_printk(KERN_ERR, "previous restart attempt failed,"
+			   " halting\n");
+		goto halt;
+	}
 
 	drv_printk(KERN_INFO, "trying IPC restart...\n");
+	already_in_restart = 1;
 
-	fd = starlet_open(dev_stm_immediate, 0);
+	fd = starlet_open_polled(dev_stm_immediate, 0, STARLET_STM_TIMEOUT);
 	if (fd < 0) {
 		drv_printk(KERN_ERR, "failed to open %s\n", dev_stm_immediate);
 		error = fd;
@@ -62,10 +69,16 @@ static void starlet_stm_common_restart(int request, u32 value)
 	}
 
 	*buf = value;
-	error = starlet_ioctl(fd, request, buf, len, buf, len);
-	if (error < 0)
+	error = starlet_ioctl_polled(fd, request, buf, len, buf, len,
+				     STARLET_STM_TIMEOUT);
+	if (error < 0) {
 		drv_printk(KERN_ERR, "ioctl %d failed\n", request);
-	starlet_close(fd);
+		starlet_close_polled(fd, STARLET_STM_TIMEOUT);
+	} else {
+halt:
+		for (;;)
+			cpu_relax();
+	}
 
 done:
 	if (error < 0)
