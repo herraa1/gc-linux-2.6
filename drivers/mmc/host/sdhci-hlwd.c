2623a54e474d8f18dedd582e0cf8fab5c7c82f91
Albert Herranz <albert_herranz@yahoo.es>
2009-10-12 20:25:33 +0200

    wii: complete rename of sdhci-mipc to sdhci-hlwd
    Signed-off-by: Albert Herranz <albert_herranz@yahoo.es>

--- drivers/mmc/host/sdhci-hlwd.c
+++ drivers/mmc/host/sdhci-hlwd.c
@@ -1,7 +1,7 @@
 /*
- * drivers/mmc/host/sdhci-mipc.c
+ * drivers/mmc/host/sdhci-hlwd.c
  *
- * Nintendo Wii Secure Digital Host Controller Interface via 'mini' IPC (mipc).
+ * Nintendo Wii Secure Digital Host Controller Interface.
  * Copyright (C) 2009 The GameCube Linux Team
  * Copyright (C) 2009 Albert Herranz
  *
@@ -30,11 +30,11 @@
 #include <asm/starlet-mini.h>
 #include "sdhci.h"
 
-#define DRV_MODULE_NAME "sdhci-mipc"
-#define DRV_DESCRIPTION "Secure Digital Host Controller Interface via 'mini'"
+#define DRV_MODULE_NAME "sdhci-hlwd"
+#define DRV_DESCRIPTION "Nintendo Wii Secure Digital Host Controller Interface"
 #define DRV_AUTHOR      "Albert Herranz"
 
-static char sdhci_mipc_driver_version[] = "0.1i";
+static char sdhci_hlwd_driver_version[] = "0.1i";
 
 #define drv_printk(level, format, arg...) \
 	printk(level DRV_MODULE_NAME ": " format , ## arg)
@@ -43,39 +43,39 @@ static char sdhci_mipc_driver_version[] = "0.1i";
 					   __func__, ## arg)
 
 
-struct sdhci_mipc_data {
+struct sdhci_hlwd_data {
 	unsigned int quirks;
 	struct sdhci_ops ops;
 };
 
-struct sdhci_mipc_host {
+struct sdhci_hlwd_host {
 	u16 xfer_mode_shadow;
 };
 
-static u32 sdhci_mipc_readl(struct sdhci_host *host, int reg)
+static u32 sdhci_hlwd_readl(struct sdhci_host *host, int reg)
 {
 	return in_be32(host->ioaddr + reg);
 }
 
-static u16 sdhci_mipc_readw(struct sdhci_host *host, int reg)
+static u16 sdhci_hlwd_readw(struct sdhci_host *host, int reg)
 {
 	return in_be16(host->ioaddr + (reg ^ 0x2));
 }
 
-static u8 sdhci_mipc_readb(struct sdhci_host *host, int reg)
+static u8 sdhci_hlwd_readb(struct sdhci_host *host, int reg)
 {
 	return in_8(host->ioaddr + (reg ^ 0x3));
 }
 
-static void sdhci_mipc_writel(struct sdhci_host *host, u32 val, int reg)
+static void sdhci_hlwd_writel(struct sdhci_host *host, u32 val, int reg)
 {
 	out_be32(host->ioaddr + reg, val);
 	udelay(5);
 }
 
-static void sdhci_mipc_writew(struct sdhci_host *host, u16 val, int reg)
+static void sdhci_hlwd_writew(struct sdhci_host *host, u16 val, int reg)
 {
-	struct sdhci_mipc_host *mipc_host = sdhci_priv(host);
+	struct sdhci_hlwd_host *hlwd_host = sdhci_priv(host);
 	int base = reg & ~0x3;
 	int shift = (reg & 0x2) * 8;
 
@@ -85,10 +85,10 @@ static void sdhci_mipc_writew(struct sdhci_host *host, u16 val, int reg)
 		 * Postpone this write, we must do it together with a
 		 * command write that is down below.
 		 */
-		mipc_host->xfer_mode_shadow = val;
+		hlwd_host->xfer_mode_shadow = val;
 		return;
 	case SDHCI_COMMAND:
-		sdhci_mipc_writel(host, val << 16 | mipc_host->xfer_mode_shadow,
+		sdhci_hlwd_writel(host, val << 16 | hlwd_host->xfer_mode_shadow,
 				  SDHCI_TRANSFER_MODE);
 		return;
 	}
@@ -97,7 +97,7 @@ static void sdhci_mipc_writew(struct sdhci_host *host, u16 val, int reg)
 	udelay(5);
 }
 
-static void sdhci_mipc_writeb(struct sdhci_host *host, u8 val, int reg)
+static void sdhci_hlwd_writeb(struct sdhci_host *host, u8 val, int reg)
 {
 	int base = reg & ~0x3;
 	int shift = (reg & 0x3) * 8;
@@ -106,30 +106,30 @@ static void sdhci_mipc_writeb(struct sdhci_host *host, u8 val, int reg)
 	udelay(5);
 }
 
-static struct sdhci_mipc_data sdhci_mipc = {
+static struct sdhci_hlwd_data sdhci_hlwd = {
 	.quirks = SDHCI_QUIRK_32BIT_DMA_ADDR |
 		  SDHCI_QUIRK_32BIT_DMA_SIZE,
 	.ops = {
-		.readl = sdhci_mipc_readl,
-		.readw = sdhci_mipc_readw,
-		.readb = sdhci_mipc_readb,
-		.writel = sdhci_mipc_writel,
-		.writew = sdhci_mipc_writew,
-		.writeb = sdhci_mipc_writeb,
+		.readl = sdhci_hlwd_readl,
+		.readw = sdhci_hlwd_readw,
+		.readb = sdhci_hlwd_readb,
+		.writel = sdhci_hlwd_writel,
+		.writew = sdhci_hlwd_writew,
+		.writeb = sdhci_hlwd_writeb,
 	},
 };
 
 
 #ifdef CONFIG_PM
 
-static int sdhci_mipc_suspend(struct of_device *ofdev, pm_message_t state)
+static int sdhci_hlwd_suspend(struct of_device *ofdev, pm_message_t state)
 {
 	struct sdhci_host *host = dev_get_drvdata(&ofdev->dev);
 
 	return mmc_suspend_host(host->mmc, state);
 }
 
-static int sdhci_mipc_resume(struct of_device *ofdev)
+static int sdhci_hlwd_resume(struct of_device *ofdev)
 {
 	struct sdhci_host *host = dev_get_drvdata(&ofdev->dev);
 
@@ -138,18 +138,18 @@ static int sdhci_mipc_resume(struct of_device *ofdev)
 
 #else
 
-#define sdhci_mipc_suspend NULL
-#define sdhci_mipc_resume NULL
+#define sdhci_hlwd_suspend NULL
+#define sdhci_hlwd_resume NULL
 
 #endif
 
-static int __devinit sdhci_mipc_probe(struct of_device *ofdev,
+static int __devinit sdhci_hlwd_probe(struct of_device *ofdev,
 				      const struct of_device_id *match)
 {
 	struct device_node *np = ofdev->node;
-	struct sdhci_mipc_data *sdhci_mipc_data = match->data;
+	struct sdhci_hlwd_data *sdhci_hlwd_data = match->data;
 	struct sdhci_host *host;
-	struct sdhci_mipc_host *mipc_host;
+	struct sdhci_hlwd_host *hlwd_host;
 	struct resource res;
 	int error;
 
@@ -163,7 +163,7 @@ static int __devinit sdhci_mipc_probe(struct of_device *ofdev,
 		goto out;
 	}
 
-	host = sdhci_alloc_host(&ofdev->dev, sizeof(*mipc_host));
+	host = sdhci_alloc_host(&ofdev->dev, sizeof(*hlwd_host));
 	if (!host) {
 		DBG("unable to allocate sdhci_host\n");
 		error = -ENODEV;
@@ -193,9 +193,9 @@ static int __devinit sdhci_mipc_probe(struct of_device *ofdev,
 	}
 
 	host->hw_name = dev_name(&ofdev->dev);
-	if (sdhci_mipc_data) {
-		host->quirks = sdhci_mipc_data->quirks;
-		host->ops = &sdhci_mipc_data->ops;
+	if (sdhci_hlwd_data) {
+		host->quirks = sdhci_hlwd_data->quirks;
+		host->ops = &sdhci_hlwd_data->ops;
 	}
 
 	error = sdhci_add_host(host);
@@ -217,7 +217,7 @@ out:
 	return error;
 }
 
-static int __devexit sdhci_mipc_remove(struct of_device *ofdev)
+static int __devexit sdhci_hlwd_remove(struct of_device *ofdev)
 {
 	struct sdhci_host *host = dev_get_drvdata(&ofdev->dev);
 
@@ -228,35 +228,35 @@ static int __devexit sdhci_mipc_remove(struct of_device *ofdev)
 	return 0;
 }
 
-static const struct of_device_id sdhci_mipc_match[] = {
-	{ .compatible = "nintendo,hollywood-sdhci", .data = &sdhci_mipc, },
+static const struct of_device_id sdhci_hlwd_match[] = {
+	{ .compatible = "nintendo,hollywood-sdhci", .data = &sdhci_hlwd, },
 	{},
 };
-MODULE_DEVICE_TABLE(of, sdhci_mipc_match);
+MODULE_DEVICE_TABLE(of, sdhci_hlwd_match);
 
-static struct of_platform_driver sdhci_mipc_driver = {
+static struct of_platform_driver sdhci_hlwd_driver = {
 	.driver.name = DRV_MODULE_NAME,
-	.match_table = sdhci_mipc_match,
-	.probe = sdhci_mipc_probe,
-	.remove = __devexit_p(sdhci_mipc_remove),
-	.suspend = sdhci_mipc_suspend,
-	.resume	= sdhci_mipc_resume,
+	.match_table = sdhci_hlwd_match,
+	.probe = sdhci_hlwd_probe,
+	.remove = __devexit_p(sdhci_hlwd_remove),
+	.suspend = sdhci_hlwd_suspend,
+	.resume	= sdhci_hlwd_resume,
 };
 
-static int __init sdhci_mipc_init(void)
+static int __init sdhci_hlwd_init(void)
 {
 	drv_printk(KERN_INFO, "%s - version %s\n", DRV_DESCRIPTION,
-		   sdhci_mipc_driver_version);
+		   sdhci_hlwd_driver_version);
 
-	return of_register_platform_driver(&sdhci_mipc_driver);
+	return of_register_platform_driver(&sdhci_hlwd_driver);
 }
-module_init(sdhci_mipc_init);
+module_init(sdhci_hlwd_init);
 
-static void __exit sdhci_mipc_exit(void)
+static void __exit sdhci_hlwd_exit(void)
 {
-	of_unregister_platform_driver(&sdhci_mipc_driver);
+	of_unregister_platform_driver(&sdhci_hlwd_driver);
 }
-module_exit(sdhci_mipc_exit);
+module_exit(sdhci_hlwd_exit);
 
 MODULE_AUTHOR(DRV_AUTHOR);
 MODULE_DESCRIPTION(DRV_DESCRIPTION);
